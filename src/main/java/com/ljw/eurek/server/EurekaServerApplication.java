805f0799a1fa4c06e9d9e15ce44a0542b8bacc3d
lijinwu1988 <sh1025462978@sina.com>
2018-04-26 11:58:58 +0800

    eureka服务端

---
+++ src/main/java/com/ljw/eurek/server/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.ljw.eurek.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(EurekaServerApplication.class, args);
+		ConfigurableEnvironment environment = context.getEnvironment();
+		
+		System.out.println("\n----------------------------------------------------------\n\t");
+		System.out.println("\t\tLocal:http://localhost:"+ environment.getProperty("server.port") +"\n\t");
+		System.out.println("\t\tapp is running");
+		System.out.println("\n----------------------------------------------------------\n\t");
+	}
+}
