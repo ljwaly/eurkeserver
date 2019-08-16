05f6ada9a1e906eb1fb368cfd1c28d8b32d77872
lijinwu1988 <sh1025462978@sina.com>
2019-08-16 09:20:48 +0800

    9003作为端口

--- src/main/java/com/ljw/eurek/server/EurekaServerApplication.java
+++ src/main/java/com/ljw/eurek/server/EurekaServerApplication.java
@@ -1,12 +1,12 @@
 package com.ljw.eurek.server;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-
+import org.springframework.boot.SpringApplication;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
@@ -19,5 +19,9 @@ public class EurekaServerApplication {
 		System.out.println("\t\tLocal:http://localhost:"+ environment.getProperty("server.port") +"\n\t");
 		System.out.println("\t\tapp is running");
 		System.out.println("\n----------------------------------------------------------\n\t");
+		
+		
+		System.out.println(SpringApplication.class.getProtectionDomain().getCodeSource().getLocation());
+
 	}
 }
